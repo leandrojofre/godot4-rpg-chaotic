979a1eda3c7034e649f242f5e781db97e3e78de5
Leandro Jofré <leandrotomasjofre@gmail.com>
2024-07-31 22:26:14 -0300

    Feat: Add collisions and foregrounds
    The tiles in the tilemap now have proper collisions with the player, and tiles 'above' the player are drawn above the player.

--- scripts/characters/player/Player.gd
+++ scripts/characters/player/Player.gd
@@ -2,17 +2,27 @@ extends CharacterBody2D
 
 @export var speed = 240
 @export var run_multiplier = 2.5
+@onready var animations = $AnimatedSprite2D
 
 func _physics_process(_delta):
 	var input_direction_x = float(Input.is_action_pressed("right")) - float(Input.is_action_pressed("left"))
 	var input_direction_y = float(Input.is_action_pressed("down")) - float(Input.is_action_pressed("up"))
+	
+	if input_direction_x < 0:
+		animations.flip_h = true
+	elif input_direction_x > 0:
+		animations.flip_h = false
 
 	var step_x = input_direction_x * speed
 	var step_y = input_direction_y * speed
 	
 	if Input.is_action_pressed("run"):
+		animations.play("run")
 		step_x *= run_multiplier
 		step_y *= run_multiplier
+	elif  Input.is_action_pressed("key-down"):
+		animations.play("walk")
+	else: animations.play("idle")
 	
 	velocity.x = step_x
 	velocity.y = step_y
